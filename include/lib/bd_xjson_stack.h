aa4465de64373f0718cb3f3d1048351c71e3032d
butdraw <shzclx123456@163.com>
2022-01-08 16:09:49 +0800

    define type of capacity and size in bd_xjson_stack as <unsigned long>

--- include/lib/bd_xjson_stack.h
+++ include/lib/bd_xjson_stack.h
@@ -1,19 +1,21 @@
 #ifndef BD_XJSON_STACK_H
 #define BD_XJSON_STACK_H
 
-#define bd_xjson_stack(type) \
+#define bd_xjson_stack(__type) \
 struct \
 { \
-    int64_t top; \
-    int64_t capacity; \
-    type* data; \
+    uint64_t top; \
+    uint64_t capacity; \
+    uint64_t size; \
+    __type* data; \
 }
 
 #define bd_xjson_stack_init(__stk, __capacity) \
 do{ \
-    __stk.top = -1; \
+    __stk.top = __capacity; \
     __stk.capacity = __capacity; \
-    __stk.data = xzmalloc((__capacity)*sizeof(*__stk.data)); \
+    __stk.size = 0; \
+    __stk.data = xzmalloc((__capacity)*sizeof(*(__stk.data))); \
 } while(0)
 
 #define bd_xjson_stack_top(__stk) \
@@ -21,23 +23,26 @@ do{ \
 
 #define bd_xjson_stack_push(__stk, __ele) \
 do{ \
-    __stk.data[__stk.top + 1] = __ele; \
-    __stk.top += 1; \
+    __stk.top = (__stk.top + 1) % (__stk.capacity + 1); \
+    __stk.data[__stk.top] = __ele; \
+    __stk.size += 1; \
 } while(0)
 
 #define bd_xjson_stack_pop(__stk) \
 do{ \
-    __stk.top -= 1; \
+    __stk.top = __stk.capacity - ((__stk.capacity - __stk.top + 1) % (__stk.capacity + 1)); \
+    __stk.size -= 1; \
 } while(0)
 
 #define bd_xjson_stack_clear(__stk) \
 do{ \
-    __stk.top = -1; \
+    __stk.top = 0; \
     __stk.capacity = 0; \
+    __stk.size = 0; \
     xfree(__stk.data); \
 } while(0)
 
 #define bd_xjson_stack_empty(__stk) \
-    (__stk.top == -1)
+    (__stk.top == __stk.capacity)
 
 #endif
